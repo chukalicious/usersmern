9ecc773fa84962cdd7376ddfc3a675ea07ade4bf
Katiuska Alicea <katiuskaalicea@gmail.com>
2024-05-16 11:07:43 -0700

    chore: Refactor userController to separate login functionality

--- backend/controller/usersController.js
+++ backend/controller/usersController.js
@@ -3,12 +3,13 @@ const Users = require('../models/userModels');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-
+// function to get all users
 const getUsers = asyncHandler(async (req, res) => {
   const users = await Users.find();
   res.json(users);
 });
 
+// function to get user by id
 const getUserById = asyncHandler(async (req, res) => {
   const user = await Users.findById(req.params.id);
   if (!user) {
@@ -18,6 +19,7 @@ const getUserById = asyncHandler(async (req, res) => {
   res.status(200).json(user);
 });
 
+// function to register a user
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
   
@@ -49,6 +51,40 @@ const registerUser = asyncHandler(async (req, res) => {
   res.status(201).json(newUser);
 });
 
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if(!email || !password){
+    res.status(400);
+    throw new Error('Please fill all the fields');
+  } 
+
+  const user = await Users.findOne({ email });
+
+  if (!user) {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+      }),
+    });
+
+
+  } else {
+    res.status(401);
+    throw new Error('Invalid Credentials');
+  }
+}
+);
+
+
 const updateProfile = asyncHandler(async (req, res) => {
   const user = await Users.findById(req.params.id);
   if (!user) {
@@ -77,4 +113,5 @@ module.exports = {
   registerUser,
   updateProfile,
   deleteProfile,
+  loginUser,
 };
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getUsers, getUserById, registerUser, updateProfile, deleteProfile} = require("../controller/usersController");
+const { getUsers, getUserById, registerUser, updateProfile, deleteProfile, loginUser} = require("../controller/usersController");
 
 router.get('/', getUsers);
 
@@ -8,9 +8,7 @@ router.post('/register', registerUser);
 
 router.route("/profile/:id").get(getUserById).put(updateProfile).delete(deleteProfile);   
 
-router.post('/login', (req, res) => {
-    res.status(200).send({ message: 'User Registered' });
-});
+router.post('/login', loginUser);
 
 
 
